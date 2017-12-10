acba1d173b9e16e0d7128d6952eb3f8242922fab
robintiwari07 <34018674+robintiwari07@users.noreply.github.com>
2017-12-10 14:09:16 +0545

    Update acceleration.py

--- acceleration.py
+++ acceleration.py
@@ -2,5 +2,5 @@ def acceleration(final_vel, initial_vel, time):
     accelerate=(final_vel-initial_vel)/time
     print("The acceleration when final velocity is {}m/s, initial velocity is {}m/s and time is {}s is: ".format(final_vel,initial_vel,time),accelerate)
 
-acceleration(40, 30, 60)
+acceleration(25, 0, 10)
 
