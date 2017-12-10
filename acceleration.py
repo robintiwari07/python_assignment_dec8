5046bc2b68185602c17c61ad7443ad75a9a4f9b9
robintiwari07 <34018674+robintiwari07@users.noreply.github.com>
2017-12-10 14:10:31 +0545

    Delete acceleration.py

--- acceleration.py
+++
@@ -1,6 +0,0 @@
-def acceleration(final_vel, initial_vel, time):
-    accelerate=(final_vel-initial_vel)/time
-    print("The acceleration when final velocity is {}m/s, initial velocity is {}m/s and time is {}s is: ".format(final_vel,initial_vel,time),accelerate)
-
-acceleration(25, 0, 10)
-
