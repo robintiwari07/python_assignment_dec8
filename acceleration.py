d2c72dd7a87176c5d68d17d46dc07128b83a3040
robintiwari07 <robin.nate22@gmail.com>
2017-12-10 14:08:12 +0545

    Program To Calculate Acceleration

---
+++ acceleration.py
@@ -0,0 +1,6 @@
+def acceleration(final_vel, initial_vel, time):
+    accelerate=(final_vel-initial_vel)/time
+    print("The acceleration when final velocity is {}m/s, initial velocity is {}m/s and time is {}s is: ".format(final_vel,initial_vel,time),accelerate)
+
+acceleration(40, 30, 60)
+
